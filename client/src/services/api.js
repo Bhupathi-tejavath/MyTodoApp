e9526f93b55a35a0b2a707bbc3d22b2fd0bc1f2c
Bhupathi Tejavath <tejavathbhupathi0@gmail.com>
2026-07-26 16:21:35 +0530

    Fix API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-const API=axios.create({
-    baseURL:"https://mytodoapp-w3v9.onrender.com",
+
+const API = axios.create({
+  baseURL: "https://mytodoapp-w3v9.onrender.com/api",
 });
-API.interceptors.request.use((config)=>{
-    const token=localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization=`Bearer ${token}`;
-    }
-    return config;
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
+
 export default API;
